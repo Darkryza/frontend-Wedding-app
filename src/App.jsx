0285b081985cd12461ae4d36e9b705b8686cb19b
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-04-04 13:56:58 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,10 @@ function App() {
             <div className="text">
               <h2>title</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
-                excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
-                architecto ullam blanditiis, rerum similique beatae quas.
-                Consectetur praesentium voluptas cum odio. Lorem ipsum dolor sit
-                amet consectetur adipisicing elit. Molestiae, quia, esse, optio
-                consectetur vitae est sunt omnis architecto consequuntur minima
-                nam. Perferendis repellendus voluptatem sint neque. Fugit
-                quibusdam fuga modi. Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Architecto omnis voluptatem commodi aliquam
-                esse nesciunt. Cum numquam consequatur perspiciatis aperiam,
-                nihil quis, suscipit explicabo omnis eum, eos voluptatem minus
-                debitis? Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Rerum, praesentium magni consequatur iste atque voluptas
-                temporibus deleniti et, ducimus reprehenderit recusandae ex
-                repellendus, aperiam ipsa maiores officia fuga cum nisi?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium amet dolor odio consectetur quia rem temporibus,
+                ipsam ipsum ex provident magni nihil eligendi nesciunt placeat
+                consequuntur quasi neque, vero quibusdam.
               </p>
             </div>
           </div>

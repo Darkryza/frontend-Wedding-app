b2d73540fbb4a0dca873a6ae351b2a98348f8c5c
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-04-04 13:43:08 +0800

    test siap kali pertama

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,93 @@ function App() {
         <div className="btn-capture-container">
           <button>Take Photo</button>
         </div>
+        <div className="contents-container">
+          <div className="content">
+            <div className="img">
+              <img
+                src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>title</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
+                architecto ullam blanditiis, rerum similique beatae quas.
+                Consectetur praesentium voluptas cum odio.
+              </p>
+            </div>
+          </div>
+          <div className="content">
+            <div className="img">
+              <img
+                src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>title</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
+                architecto ullam blanditiis, rerum similique beatae quas.
+                Consectetur praesentium voluptas cum odio.
+              </p>
+            </div>
+          </div>
+          <div className="content">
+            <div className="img">
+              <img
+                src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>title</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
+                architecto ullam blanditiis, rerum similique beatae quas.
+                Consectetur praesentium voluptas cum odio.
+              </p>
+            </div>
+          </div>
+          <div className="content">
+            <div className="img">
+              <img
+                src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>title</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
+                architecto ullam blanditiis, rerum similique beatae quas.
+                Consectetur praesentium voluptas cum odio.
+              </p>
+            </div>
+          </div>
+          <div className="content">
+            <div className="img">
+              <img
+                src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
+                alt="image"
+              />
+            </div>
+            <div className="text">
+              <h2>title</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+                excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
+                architecto ullam blanditiis, rerum similique beatae quas.
+                Consectetur praesentium voluptas cum odio.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

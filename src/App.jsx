07fbba9e3600b9896ce7ff28e99bfce281540483
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-04-04 13:56:07 +0800

    prototype 1st

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,18 @@ function App() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
                 excepturi. Totam cum, iure temporibus soluta autem dolorum ipsa
                 architecto ullam blanditiis, rerum similique beatae quas.
-                Consectetur praesentium voluptas cum odio.
+                Consectetur praesentium voluptas cum odio. Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Molestiae, quia, esse, optio
+                consectetur vitae est sunt omnis architecto consequuntur minima
+                nam. Perferendis repellendus voluptatem sint neque. Fugit
+                quibusdam fuga modi. Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Architecto omnis voluptatem commodi aliquam
+                esse nesciunt. Cum numquam consequatur perspiciatis aperiam,
+                nihil quis, suscipit explicabo omnis eum, eos voluptatem minus
+                debitis? Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Rerum, praesentium magni consequatur iste atque voluptas
+                temporibus deleniti et, ducimus reprehenderit recusandae ex
+                repellendus, aperiam ipsa maiores officia fuga cum nisi?
               </p>
             </div>
           </div>

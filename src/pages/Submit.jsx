cfeca5675aeded1f910c9cb7074bd94578376f8e
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-04-05 16:49:20 +0800

    siapkan submit ucapan

--- src/pages/Submit.jsx
+++ src/pages/Submit.jsx
@@ -7,17 +7,29 @@ function Submit() {
 
   return (
     <div className="submit-container">
-      <div className="img-container">
-        {image ? (
-          <img src={image} alt="captured" />
-        ) : (
-          // <img
-          //   src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
-          //   alt="captured"
-          // />
-          <p>No image found. Please go back and take a photo.</p>
-        )}
+      {image ? (
+        <div className="img-container">
+          {/* <img src={image} alt="captured" /> */}
+          <img
+            src="https://comptonhouseoffashion.co.uk/content/uploads/2019/11/Picture1-1.png"
+            alt="captured"
+          />
+        </div>
+      ) : (
+        <p>No image found. Please go back and take a photo.</p>
+      )}
+      <div className="title">
+        <h1>Berikan Ucapan buat pengantin</h1>
       </div>
+      <form className="submit-form-container">
+        <input type="text" name="name" placeholder="Name" />
+        <label htmlFor="ucapan">Ucapan</label>
+        <textarea
+          name="ucapan"
+          id="ucapan"
+          placeholder="Berikan ucapan anda di sini"
+        ></textarea>
+      </form>
     </div>
   );
 }
